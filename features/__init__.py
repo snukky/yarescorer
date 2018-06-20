dbecf19fdf252c00b1677526db0831d3cfb7f33e
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-06-20 13:09:42 +0000

    Remove 'from X import Y'

--- features/__init__.py
+++ features/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from os.path import dirname, basename, isfile
 import glob
 
-modules = glob.glob(dirname(__file__) + "/*.py")
-__all__ = [basename(f)[:-3] for f in modules
-           if isfile(f) and not f.endswith('__init__.py')]
+modules = glob.glob(os.path.dirname(__file__) + "/*.py")
+__all__ = [os.path.basename(f)[:-3] for f in modules
+           if os.path.isfile(f) and not f.endswith('__init__.py')]
